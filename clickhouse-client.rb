6f6e33cdab2ae291c539307dbfb6c2436254536c
Vladimir Chebotarev <chebotarev@yandex-team.ru>
2016-11-16 05:34:34 +0300

    update

--- clickhouse-client.rb
+++ clickhouse-client.rb
@@ -1,8 +1,8 @@
 class ClickhouseClient < Formula
 	desc "ClickHouse command-line client"
 	homepage "https://clickhouse.yandex/"
-	url "https://github.yandex-team.ru/chebotarev/ClickHouse/releases/download/1.1.54055/clickhouse-client-1.1.54055.tar.gz"
-	sha256 "1a2084d8078827682d5ee45e27354456f1b7ab355823fb5249dc64c7ef70b806"
+	url "https://github.com/excitoon/ClickHouse/releases/download/1.1.54059/clickhouse-client-1.1.54059.tar.gz"
+	sha256 "64eafb192386dede93f26ee2f980bfa37248fbd131278f9b0ef9f3a821eb603c"
 
 	bottle :unneeded
 
